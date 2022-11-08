cabbaf3bceee669e88d39e741f26dc14d1640a2b
Alina <alinavik93@gmail.com>
2022-11-08 20:50:23 +0500

    add task

--- result/Program.cs
+++ result/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace FinalControl {
+    class Program {
+        static void Main(string[] args)
+        {
+            Input input = new Input();
+            input.StartProgram();
+            
+        }
+    }
+}
+
+
+
